1dfc299050d1a405198cf0d3293e18d6696c1b53
Kevin <87081548+kevzbldai@users.noreply.github.com>
2025-04-24 17:43:07 +0800

    Permission status (#50)
    * Add permission status js method
    
    * Specific permission status types

--- src/plugins/permissions.ts
+++ src/plugins/permissions.ts
@@ -1,12 +1,27 @@
 import { CallbackParams } from '../types';
 import { addCommand, addCommandCallback } from '../utils';
 
+type PermissionType =
+  | 'Notifications'
+  | 'Camera'
+  | 'Contacts'
+  | 'Microphone'
+  | 'LocationWhenInUse'
+  | 'LocationAlways'
+  | 'AppTrackingTransparency'
+  | 'PhotoLibrary';
+
+type PermissionStatus = 'undetermined' | 'granted' | 'denied';
+
 const permissions = {
   requestPhoneCallMgmt: function (params: CallbackParams) {
     if (params?.callback) {
       return addCommandCallback('median://permissions/requestPhoneCallMgmt', params);
     }
     addCommand('median://permissions/requestPhoneCallMgmt', params);
+  },
+  status: function (permissions?: PermissionType[]) {
+    return addCommandCallback<Record<string, PermissionStatus>>('median://permissions/status', { permissions });
   },
 };
 

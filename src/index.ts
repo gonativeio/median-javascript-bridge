c500da3ac404be54b970d6870a12f06eaab9e9e8
kevz <kevz@bld.ai>
2026-01-15 18:46:42 +0800

    Use namespace and expose health bridge types

--- src/index.ts
+++ src/index.ts
@@ -1,186 +1,189 @@
+import * as commands from './commands';
 import * as plugins from './plugins';
-import { android, general, ios } from './commands';
 import { BranchInitializedData } from './plugins/branch';
 import { InAppPurchaseInfoReadyData } from './plugins/iap';
 import { ShareToAppData } from './plugins/share';
-import { AnyData } from './types';
 import { createTempFunctionName, setMedianCallback, setSubscription } from './utils';
 
-class Median {
-  #listeners: Record<string, Record<string, (...args: AnyData) => void>> = {};
+import { HealthBridge as HealthBridgeType } from './types/healthBridge';
 
-  #addListener = <T>(functionName: string, callback: (data: T) => void) => {
+export namespace Median {
+  ///////////////////////////////
+  //          Internal         //
+  ///////////////////////////////
+  const listeners: Record<string, Record<string, (...args: any[]) => void>> = {};
+
+  const addListener = <T>(functionName: string, callback: (data: T) => void) => {
     const functionId = createTempFunctionName(functionName);
 
     if (typeof callback !== 'function') {
       return functionId;
     }
 
-    this.#listeners[functionName] = this.#listeners[functionName] || {};
-    const callbackFunctions = this.#listeners[functionName];
-    callbackFunctions[functionId] = callback;
+    listeners[functionName] = listeners[functionName] || {};
+    listeners[functionName][functionId] = callback;
 
-    setMedianCallback(functionName, callbackFunctions);
+    setMedianCallback(functionName, listeners[functionName]);
     setSubscription(functionName, true);
 
     return functionId;
   };
 
-  #removeListener = (functionName: string, functionId: string) => {
-    if (!functionName || !functionId) {
-      return;
-    }
+  const removeListener = (functionName: string, functionId: string) => {
+    if (!functionName || !functionId) return;
 
-    this.#listeners[functionName] = this.#listeners[functionName] || {};
-    const callbackFunctions = this.#listeners[functionName];
-    delete callbackFunctions[functionId];
+    listeners[functionName] = listeners[functionName] || {};
+    delete listeners[functionName][functionId];
 
-    setMedianCallback(functionName, callbackFunctions);
-    if (Object.keys(callbackFunctions).length === 0) {
+    setMedianCallback(functionName, listeners[functionName]);
+    if (Object.keys(listeners[functionName]).length === 0) {
       setSubscription(functionName, false);
     }
   };
 
-  #createListenerProp = <T = void>(functionName: string) => {
-    return {
-      addListener: (callback: (data: T) => void) => {
-        return this.#addListener<T>(functionName, callback);
-      },
-      removeListener: (functionId: string) => {
-        return this.#removeListener(functionName, functionId);
-      },
-    };
-  };
+  const createListenerProp = <T = void>(functionName: string) => ({
+    addListener: (callback: (data: T) => void) => addListener<T>(functionName, callback),
+    removeListener: (functionId: string) => removeListener(functionName, functionId),
+  });
 
-  // General
-  ios = ios;
-  android = android;
-  camera = general.camera;
-  clipboard = general.clipboard;
-  config = general.config;
-  connectivity = general.connectivity;
-  deviceInfo = general.deviceInfo;
-  internalExternal = general.internalExternal;
-  keyboard = general.keyboard;
-  nativebridge = general.nativebridge;
-  navigationLevels = general.navigationLevels;
-  navigationMaxWindows = general.navigationMaxWindows;
-  navigationTitles = general.navigationTitles;
-  open = general.open;
-  registration = general.registration;
-  run = general.run;
-  screen = general.screen;
-  share = general.share;
-  sidebar = general.sidebar;
-  statusbar = general.statusbar;
-  tabNavigation = general.tabNavigation;
-  webview = general.webview;
-  window = general.window;
+  ///////////////////////////////
+  //      General Commands     //
+  ///////////////////////////////
+  export const ios = commands.ios;
+  export const android = commands.android;
+  export const camera = commands.general.camera;
+  export const clipboard = commands.general.clipboard;
+  export const config = commands.general.config;
+  export const connectivity = commands.general.connectivity;
+  export const deviceInfo = commands.general.deviceInfo;
+  export const internalExternal = commands.general.internalExternal;
+  export const keyboard = commands.general.keyboard;
+  export const nativebridge = commands.general.nativebridge;
+  export const navigationLevels = commands.general.navigationLevels;
+  export const navigationMaxWindows = commands.general.navigationMaxWindows;
+  export const navigationTitles = commands.general.navigationTitles;
+  export const open = commands.general.open;
+  export const registration = commands.general.registration;
+  export const run = commands.general.run;
+  export const screen = commands.general.screen;
+  export const share = commands.general.share;
+  export const sidebar = commands.general.sidebar;
+  export const statusbar = commands.general.statusbar;
+  export const tabNavigation = commands.general.tabNavigation;
+  export const webview = commands.general.webview;
+  export const window = commands.general.window;
 
-  // Plugins
-  adjust = plugins.adjust;
-  admob = plugins.admob;
-  ageSafety = plugins.ageSafety;
-  appreview = plugins.appreview;
-  appsflyer = plugins.appsflyer;
-  auth = plugins.auth;
-  auth0 = plugins.auth0;
-  autorefresh = plugins.autorefresh;
-  backgroundLocation = plugins.backgroundLocation;
-  backgroundMedia = plugins.backgroundMedia;
-  barcode = plugins.barcode;
-  beacon = plugins.beacon;
-  braze = plugins.braze;
-  branch = plugins.branch;
-  calendar = plugins.calendar;
-  card_io = plugins.card_io;
-  contacts = plugins.contacts;
-  cordial = plugins.cordial;
-  documentScanner = plugins.documentScanner;
-  downloads = plugins.downloads;
-  esmiley = plugins.esmiley;
-  facebook = plugins.facebook;
-  firebaseAnalytics = plugins.firebaseAnalytics;
-  firebaseCrashlytics = plugins.firebaseCrashlytics;
-  grow = plugins.grow;
-  haptics = plugins.haptics;
-  healthBridge = plugins.healthBridge;
-  iap = plugins.iap;
-  intentedge = plugins.intentedge;
-  intercom = plugins.intercom;
-  iterable = plugins.iterable;
-  kaltura = plugins.kaltura;
-  keychainSwift = plugins.keychainSwift;
-  klaviyo = plugins.klaviyo;
-  localpreferences = plugins.localpreferences;
-  modal = plugins.modal;
-  moengage = plugins.moengage;
-  moxo = plugins.moxo;
-  msdynamics = plugins.msdynamics;
-  nfc = plugins.nfc;
-  onesignal = plugins.onesignal;
-  opentok = plugins.opentok;
-  permissions = plugins.permissions;
-  plaid = plugins.plaid;
-  purchase = plugins.iap.purchase;
-  revenueCat = plugins.revenueCat;
-  socialLogin = plugins.socialLogin;
-  socialShare = plugins.socialShare;
-  storage = {
+  ///////////////////////////////
+  //          Plugins          //
+  ///////////////////////////////
+  export const adjust = plugins.adjust;
+  export const admob = plugins.admob;
+  export const ageSafety = plugins.ageSafety;
+  export const appreview = plugins.appreview;
+  export const appsflyer = plugins.appsflyer;
+  export const auth = plugins.auth;
+  export const auth0 = plugins.auth0;
+  export const autorefresh = plugins.autorefresh;
+  export const backgroundLocation = plugins.backgroundLocation;
+  export const backgroundMedia = plugins.backgroundMedia;
+  export const barcode = plugins.barcode;
+  export const beacon = plugins.beacon;
+  export const braze = plugins.braze;
+  export const branch = plugins.branch;
+  export const calendar = plugins.calendar;
+  export const card_io = plugins.card_io;
+  export const contacts = plugins.contacts;
+  export const cordial = plugins.cordial;
+  export const documentScanner = plugins.documentScanner;
+  export const downloads = plugins.downloads;
+  export const esmiley = plugins.esmiley;
+  export const facebook = plugins.facebook;
+  export const firebaseAnalytics = plugins.firebaseAnalytics;
+  export const firebaseCrashlytics = plugins.firebaseCrashlytics;
+  export const grow = plugins.grow;
+  export const haptics = plugins.haptics;
+  export const healthBridge = plugins.healthBridge;
+  export const iap = plugins.iap;
+  export const intentedge = plugins.intentedge;
+  export const intercom = plugins.intercom;
+  export const iterable = plugins.iterable;
+  export const kaltura = plugins.kaltura;
+  export const keychainSwift = plugins.keychainSwift;
+  export const klaviyo = plugins.klaviyo;
+  export const localpreferences = plugins.localpreferences;
+  export const modal = plugins.modal;
+  export const moengage = plugins.moengage;
+  export const moxo = plugins.moxo;
+  export const msdynamics = plugins.msdynamics;
+  export const nfc = plugins.nfc;
+  export const onesignal = plugins.onesignal;
+  export const opentok = plugins.opentok;
+  export const permissions = plugins.permissions;
+  export const plaid = plugins.plaid;
+  export const purchase = plugins.iap.purchase;
+  export const revenueCat = plugins.revenueCat;
+  export const socialLogin = plugins.socialLogin;
+  export const socialShare = plugins.socialShare;
+  export const storage = {
     app: plugins.localpreferences.nonpersistent,
     cloud: plugins.localpreferences.persistent,
     filesystem: plugins.localpreferences.filesystem,
   };
-  twilio = plugins.twilio;
-  webScreenshot = plugins.webScreenshot;
-  zoom = plugins.zoom;
+  export const twilio = plugins.twilio;
+  export const webScreenshot = plugins.webScreenshot;
+  export const zoom = plugins.zoom;
 
-  // Median Functions
-  isNativeApp = () => {
+  ///////////////////////////////
+  //         Functions         //
+  ///////////////////////////////
+  export const isNativeApp = () => {
+    const window = globalThis.window;
     return !!window?.webkit?.messageHandlers?.JSBridge || !!window?.JSBridge;
   };
 
-  getPlatform = async () => {
-    if (!this.isNativeApp()) {
-      return 'web';
-    }
-    const deviceInfo = await general.deviceInfo();
-    return deviceInfo?.platform;
+  export const getPlatform = async () => {
+    if (!isNativeApp()) return 'web';
+    const info = await commands.general.deviceInfo();
+    return info?.platform;
   };
 
-  onReady = (callback: () => void) => {
-    if (typeof callback === 'function') {
-      let callbackFunction: (() => void) | null = callback;
-      let counter = 0;
+  export const onReady = (callback: () => void) => {
+    if (typeof callback !== 'function') return;
 
-      const intervalFunction = setInterval(() => {
-        if (this.isNativeApp() && callbackFunction) {
-          callbackFunction();
-          callbackFunction = null;
-          return;
-        }
+    let currentCallback: (() => void) | null = callback;
+    let counter = 0;
 
-        counter += 1;
-        if (counter >= 20 || !callbackFunction) {
-          clearInterval(intervalFunction);
-          return;
-        }
-      }, 500);
-    }
+    const interval = setInterval(() => {
+      if (isNativeApp() && currentCallback) {
+        currentCallback();
+        currentCallback = null;
+      }
+
+      counter++;
+      if (counter >= 20 || !currentCallback) {
+        clearInterval(interval);
+      }
+    }, 500);
   };
 
-  appsFlyerConversionData = this.#createListenerProp<AnyData>('_median_appsflyer_cd_success');
-  appResumed = this.#createListenerProp('_median_app_resumed');
-  branchInitialized = this.#createListenerProp<BranchInitializedData>('_median_branch_initialized');
-  deviceShake = this.#createListenerProp('_median_device_shake');
-  iapInfoReady = this.#createListenerProp<InAppPurchaseInfoReadyData>('_median_info_ready');
-  iapPurchases = this.#createListenerProp<AnyData>('_median_iap_purchases');
-  oneSignalPushOpened = this.#createListenerProp<AnyData>('_median_onesignal_push_opened');
-  shareToApp = this.#createListenerProp<ShareToAppData>('_median_share_to_app');
+  ///////////////////////////////
+  //           Events          //
+  ///////////////////////////////
+  export const appsFlyerConversionData = createListenerProp<any>('_median_appsflyer_cd_success');
+  export const appResumed = createListenerProp('_median_app_resumed');
+  export const branchInitialized = createListenerProp<BranchInitializedData>('_median_branch_initialized');
+  export const deviceShake = createListenerProp('_median_device_shake');
+  export const iapInfoReady = createListenerProp<InAppPurchaseInfoReadyData>('_median_info_ready');
+  export const iapPurchases = createListenerProp<any>('_median_iap_purchases');
+  export const oneSignalPushOpened = createListenerProp<any>('_median_onesignal_push_opened');
+  export const shareToApp = createListenerProp<ShareToAppData>('_median_share_to_app');
 
-  jsNavigation = {
-    url: this.#createListenerProp<{ url: string }>('_median_url_changed'),
-  };
+  ///////////////////////////////
+  //           Types           //
+  ///////////////////////////////
+  export namespace Plugins {
+    export import HealthBridge = HealthBridgeType;
+  }
 }
 
-export default new Median();
+export default Median;
--- src/plugins/healthBridge.ts
+++ src/plugins/healthBridge.ts
@@ -1,101 +1,25 @@
-import { CallbackData, CallbackParams } from '../types';
+import { HealthBridge } from '../types/healthBridge';
 import { addCommandCallback } from '../utils';
-
-/**
- * Supported HealthBridge data types.
- */
-type HealthBridgeDataType =
-  | 'activeEnergy'
-  | 'bloodGlucose'
-  | 'bloodPressure'
-  | 'bmi'
-  | 'bodyFat'
-  | 'bodyTemperature'
-  | 'calorieIntake'
-  | 'distance'
-  | 'elevationGain'
-  | 'exerciseTime'
-  | 'heartRate'
-  | 'heartRateVariability'
-  | 'height'
-  | 'menstruation'
-  | 'nutrition'
-  | 'oxygenSaturation'
-  | 'respiratoryRate'
-  | 'restingHeartRate'
-  | 'sleep'
-  | 'speed'
-  | 'steps'
-  | 'totalCaloriesBurned'
-  | 'vo2Max'
-  | 'waterIntake'
-  | 'weight';
-
-/**
- * Represents a single HealthBridge data item.
- */
-type HealthBridgeGetDataItem = {
-  /** Numeric value or key-value pairs (e.g. nutrition breakdown) */
-  value: number | Record<string, number>;
-  /** Timestamp for point-in-time data */
-  timestamp?: string;
-  /** Start time for interval-based data */
-  start?: string;
-  /** End time for interval-based data */
-  end?: string;
-};
-
-/**
- * HealthBridge data response.
- */
-type HealthBridgeGetData = {
-  /** Health data grouped by data type */
-  data: Record<HealthBridgeDataType, HealthBridgeGetDataItem[]>;
-};
-
-/**
- * Parameters for fetching HealthBridge data.
- */
-type HealthBridgeGetDataParams = CallbackParams<HealthBridgeGetData> & {
-  /** Health data types to retrieve */
-  dataTypes: HealthBridgeDataType[];
-  /** ISO string representing the start date */
-  startDate: string;
-  /** ISO string representing the end date */
-  endDate: string;
-  /** Optional data aggregation bucket. Default is raw. */
-  bucket?: 'raw' | 'minute' | 'hour' | 'day';
-};
 
 /**
  * HealthBridge plugin for Median.
  * Provides access to health and fitness data with permission handling.
  */
-const healthBridge = {
+export const healthBridge = {
   /**
    * Request user permission for specific health data types.
-   *
-   * @param dataTypes - List of health data types to request access for.
-   * @returns A promise resolving with the permission request result.
    */
-  requestPermissions: function (dataTypes: HealthBridgeDataType[]) {
-    return addCommandCallback<CallbackData>('median://healthBridge/requestPermissions', {
+  requestPermissions: function (dataTypes: HealthBridge.DataType[]) {
+    return addCommandCallback<HealthBridge.RequestPermissionsResponse>('median://healthBridge/requestPermissions', {
       dataTypes,
     });
   },
-  
+
   /**
    * Retrieve health data for the specified data types and time range.
-   *
-   * @param params - Parameters for fetching health data.
-   * @param params.dataTypes - Health data types to retrieve.
-   * @param params.startDate - Start date (ISO string).
-   * @param params.endDate - End date (ISO string).
-   * @param params.bucket - Optional aggregation bucket.
-   * @returns A promise resolving with the requested health data.
    */
-  getData: function (params: HealthBridgeGetDataParams) {
-    return addCommandCallback<HealthBridgeGetData>('median://healthBridge/getData', params);
+  getData: function (params: HealthBridge.GetDataParams) {
+    return addCommandCallback<HealthBridge.GetDataResponse>('median://healthBridge/getData', params);
   },
 };
 
